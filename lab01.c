53ba29622d1698a2f8f791e78330e73ea3773dad
Carlos Guilherme <52669963+Carguilhos@users.noreply.github.com>
2019-11-16 15:27:22 -0300

    Judge01 com enunciado

--- lab01.c
+++ lab01.c
@@ -1,7 +1,6 @@
 /*                       Gerenciamento de Empréstimo de Livros
 
-Prof. Valério Rosset – UNIFESP, Unidade Pq. Tecnológico, sala 05
-E-mail: vrosset@unifesp.br
+Prof. Valério Rosset – UNIFESP
 
 1 DEFINIÇÃO DO PROBLEMA
 
